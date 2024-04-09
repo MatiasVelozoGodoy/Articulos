fbb63ca235116625a2867f6c2c835ce7d88145d8
Matias Velozo <matias.velozo.4c@gmail.com>
2024-04-09 15:42:29 -0300

    mejorando colores del datagridview

--- TPFinalNivel2_VelozoGodoy/Dominio/Articulos.cs
+++ TPFinalNivel2_VelozoGodoy/Dominio/Articulos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,10 @@ namespace Dominio
     public class Articulos
     {
         public int Id { get; set; }
+        [DisplayName("Código")]
         public string Codigo { get; set; }
         public string Nombre { get; set; }
+        [DisplayName("Descripción")]
         public string Descripcion { get; set; }
         public string UrlImagen { get; set; }
         public Marcas Marca { get; set; }
